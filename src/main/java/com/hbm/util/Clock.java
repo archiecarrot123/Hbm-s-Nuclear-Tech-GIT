723606c8ccc3472ce363d94b777c5608ed429384
crumpetime <121749023+crumpetime@users.noreply.github.com>
2025-04-16 21:48:33 +1000

    hgkjfjfsdkhjfghhkjsdafgfhjfjs

---
+++ src/main/java/com/hbm/util/Clock.java
@@ -0,0 +1,6 @@
+package com.hbm.util;
+public class Clock {
+    private static long Timems;
+    public static void Update(){
+    time_ms = System.currentTimeMillis();
+    }
--- src/main/java/com/hbm/util/clock.java
+++
@@ -1,6 +0,0 @@
-package com.hbm.util;
-public class clock {
-    private static long time_ms;
-    public static void update(){
-    time_ms = System.currentTimeMillis();
-    }
